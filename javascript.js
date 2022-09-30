38f64f382e60e6b3a536b8f01bcf1b57e2e15b68
Harris Nguyen <hainx1610@gmail.com>
2022-09-30 18:11:57 +0700

    create function to play one round of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -2,4 +2,34 @@ const choices = ["Rock", "Paper", "Scissors"];
 const getComputerChoice = choiceList => {
     return choiceList[Math.floor(Math.random() * choiceList.length)];
 }
-console.log(getComputerChoice(choices));
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return("Draw!")
+    } else {
+        if (playerSelection.toLowerCase() === "rock") {
+            if (computerSelection === "Paper") {
+                return(`You lost! ${computerSelection} beats ${playerSelection}.`);
+            } else {
+                return(`You won! ${playerSelection} beats ${computerSelection}`);
+            }
+        } else if (playerSelection.toLowerCase() === "paper") {
+            if (computerSelection === "Scissors") {
+                return(`You lost! ${computerSelection} beats ${playerSelection}.`);
+            } else {
+                return(`You won! ${playerSelection} beats ${computerSelection}`);
+            }
+        } else if (playerSelection.toLowerCase() === "scissors") {
+            if (computerSelection === "Rock") {
+                return(`You lost! ${computerSelection} beats ${playerSelection}.`);
+            } else {
+                return(`You won! ${playerSelection} beats ${computerSelection}`);
+            }
+        } else {
+            return("Wrong input!")
+        }
+    }
+}
+
+console.log(playRound((prompt("Pick your poison!")), getComputerChoice(choices)));
+
