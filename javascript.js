af7a8e7c363f043af8d936147e42deb428750ed2
Harris Nguyen <hainx1610@gmail.com>
2022-09-30 18:53:15 +0700

    add function to play 5 rounds and give final result

--- javascript.js
+++ javascript.js
@@ -3,26 +3,35 @@ const getComputerChoice = choiceList => {
     return choiceList[Math.floor(Math.random() * choiceList.length)];
 }
 
+let computerScore = 0;
+let playerScore = 0;
+
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return("Draw!")
     } else {
         if (playerSelection.toLowerCase() === "rock") {
             if (computerSelection === "Paper") {
+                computerScore++;
                 return(`You lost! ${computerSelection} beats ${playerSelection}.`);
             } else {
+                playerScore++;
                 return(`You won! ${playerSelection} beats ${computerSelection}`);
             }
         } else if (playerSelection.toLowerCase() === "paper") {
             if (computerSelection === "Scissors") {
+                computerScore++;
                 return(`You lost! ${computerSelection} beats ${playerSelection}.`);
             } else {
+                playerScore++;
                 return(`You won! ${playerSelection} beats ${computerSelection}`);
             }
         } else if (playerSelection.toLowerCase() === "scissors") {
             if (computerSelection === "Rock") {
+                computerScore++;
                 return(`You lost! ${computerSelection} beats ${playerSelection}.`);
             } else {
+                playerScore++;
                 return(`You won! ${playerSelection} beats ${computerSelection}`);
             }
         } else {
@@ -31,5 +40,18 @@ const playRound = (playerSelection, computerSelection) => {
     }
 }
 
-console.log(playRound((prompt("Pick your poison!")), getComputerChoice(choices)));
+const game = () => {
+    
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound((prompt("Pick your poison!")), getComputerChoice(choices)));
+    }
+    if (computerScore === playerScore) {
+        console.log("Final result: Draw!")
+    } else if (computerScore > playerScore) {
+        console.log("Final: You've lost...")
+    } else {
+        console.log("Final: You've won!")
+    }
+}
 
+game();
